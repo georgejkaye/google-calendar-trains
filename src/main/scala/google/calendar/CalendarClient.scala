f77c2acfba5a709af211211cd4a7525da907701d
George Kaye <georgejkaye@gmail.com>
2026-02-23 22:21:10 +0000

    refactor: use build in bearer auth method

--- src/main/scala/google/calendar/CalendarClient.scala
+++ src/main/scala/google/calendar/CalendarClient.scala
@@ -4,7 +4,6 @@ import sttp.client4.quick.*
 import spray.json.*
 
 import google.calendar.EventProtocol.format
-import google.auth.RequestAuthHeaderAdder
 
 class CalendarClient(baseUrl: String) extends ICalendarClient:
   def insertCalendarEvent(
@@ -17,7 +16,7 @@ class CalendarClient(baseUrl: String) extends ICalendarClient:
       basicRequest
         .post(url)
         .body(event.toJson.prettyPrint)
-        .addAuthHeader(accessToken)
+        .auth
+        .bearer(accessToken)
         .send()
         .body
-    println(response)

6f97d99f55cd1ef18e9fab049a055a8297551f2b
George Kaye <georgejkaye@gmail.com>
2026-02-24 23:04:36 +0000

    feat: make main method not messing around

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,11 @@
 import utils.readFromFile
-import google.auth.AuthClient
-import google.calendar.CalendarClient
+import google.auth.GoogleAuthClient
+import google.calendar.GoogleCalendarClient
 import google.calendar.Event
 import google.calendar.EventTimeProtocol
 import google.calendar.EventTime
 import com.github.nscala_time.time.Imports._
 import rtt.client.RttClient
-import rtt.interactive.getStationDepartureFromList
 
 val accountsOauthBaseUrl = "https://accounts.google.com/o/oauth2/v2"
 val oauthApiBaseUrl = "https://oauth2.googleapis.com"
@@ -15,20 +14,20 @@ val oauthApiBaseUrl = "https://oauth2.googleapis.com"
 def main(): Unit =
   val config = loadConfig()
 
-  val authClient = AuthClient(
+  val authClient = GoogleAuthClient(
     accountsOauthBaseUrl,
     oauthApiBaseUrl,
     config.clientId,
     config.clientSecret,
     "google_tokens.json"
   )
-  val calendarClient = CalendarClient("https://www.googleapis.com/calendar/v3")
+  val calendarClient = GoogleCalendarClient(
+    "https://www.googleapis.com/calendar/v3"
+  )
 
   val accessToken = authClient.getAccessToken()
 
   val rttClient =
     RttClient("https://api.rtt.io/api/v1", config.rttUser, config.rttApiKey)
 
-  val stationDepartures =
-    rttClient.getDeparturesFromStation("BHM", DateTime.now())
-  println(getStationDepartureFromList(stationDepartures))
+  runProcess(config, authClient, calendarClient, rttClient)
